cf6e5e0f633c8d6ef95c91576438149223ae8058
Sıla Öztürk <sila.ozturk@stu.rumeli.edu.tr>
2025-11-12 20:27:22 +0300

    proje açıklamaları eklendi

--- portfolioplus/src/app/project/page.tsx
+++ portfolioplus/src/app/project/page.tsx
@@ -8,15 +8,15 @@ const ProjectPage = () => {
             <p className="text-center text-lg text-neutral-800">
                 Each one represents a step forward in combining creativity with functionality.
             </p>
-            <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 md:px-10 gap-10 items-center justify-center py-10">
-                <section className="bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
+            <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 md:px-10 gap-10 items-center justify-center py-10 text-lg text-neutral-800">
+                <section className="bg-white text-center justify-center items-center shadow-2xl hover:shadow-4xl transition-shadow duration-300 rounded-2xl">
                     <img
                         src="biletSatisSistemi.png"
                         alt="biletSatisSistemi Görseli"
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl"
                     ></img>
-                    <h2>Bilet Satış Sistemi</h2>
-                    <p>Bilet Satıs sistemi açıklaması</p>
+                    <h2 className="font-bold text-xl mt-2">Bilet Satış Sistemi</h2>
+                    <p className="px-2">A desktop application developed in Java that allows users to view theater performances, select seats, and purchase tickets. The system includes key features such as seat availability tracking, ticket reservation, and user management. It offers a user-friendly interface designed to provide a smooth and efficient ticket purchasing experience.</p>
                     <button>Detayları Gör</button>
                 </section>
                 <section className="bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
@@ -26,8 +26,8 @@ const ProjectPage = () => {
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl"
                     >
                     </img>
-                    <h2>My Portfolio</h2>
-                    <p>Portfolio açıklaması</p>
+                    <h2 className="font-bold text-xl mt-2">My Portfolio</h2>
+                    <p className="px-2">A modern and responsive portfolio website built with Next.js and Tailwind CSS. The project showcases personal projects, skills, and contact information through a clean and user-friendly design. It focuses on performance, simplicity, and mobile responsiveness to create a professional digital identity.</p>
                     <button>Detayları Gör</button>
                 </section>
                 <section className=" bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
@@ -37,8 +37,8 @@ const ProjectPage = () => {
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl"
                     >
                     </img>
-                    <h2>AutoFixParts</h2>
-                    <p>AutoFixParts açıklaması</p>
+                    <h2 className="font-bold text-xl mt-2">Ring Oto Far</h2>
+                    <p className="px-2">A modern, performance-focused website built with Next.js and Tailwind CSS. The site showcases the brand’s products and services through a clean and user-friendly design. It offers both Turkish and English language options to reach a wider audience. With fast loading speed, responsive layout, and minimalist interface, it delivers a strong digital experience.</p>
                     <button>Detayları Gör</button>
                 </section>
                 <section className=" bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
@@ -48,9 +48,9 @@ const ProjectPage = () => {
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl"
                     >
                     </img>
-                    <h2>insurance Company</h2>
-                    <p>insurance Company açıklaması</p>
-                    <button>Detayları Gör</button>
+                    <h2 className="font-bold text-xl mt-2">Gökmen Sigorta</h2>
+                    <p className="px-2">insurance Company açıklaması</p>
+                    <button>A modern, user-focused website built with Next.js and Tailwind CSS. It presents the insurance services offered and enables users to request quotes based on their selected insurance type. With its fast performance, responsive layout, and clean design, it provides a reliable digital experience that simplifies insurance processes.</button>
                 </section>
                 <section className=" bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
                     <img
@@ -59,19 +59,19 @@ const ProjectPage = () => {
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl"
                     >
                     </img>
-                    <h2>Google Docs Clone</h2>
-                    <p>Google Docs Clone açıklaması</p>
+                    <h2 className="font-bold text-xl mt-2">Google Docs Clone</h2>
+                    <p className="px-2">A real-time document editing application built with Next.js, Tailwind CSS, and Convex database. Users can sign in to create, edit, and securely store their documents in the cloud. With its modern design and fast performance, it delivers a collaborative experience similar to Google Docs.</p>
                     <button>Detayları Gör</button>
                 </section>
-                <section className=" bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
+                <section className="bg-white text-center justify-center items-center shadow-2xl rounded-2xl">
                     <img
                         src="derslikTakipSistemi.png"
                         alt="Derslik Takip Sistemi Görseli"
                         className="w-full h-40 md:h-60 lg:h-72 2xl:h-80 rounded-t-2xl" 
                     >
                     </img>
-                    <h2>Derslik Takip Sistemi Clone</h2>
-                    <p>Derslik Takip Sistemi açıklaması</p>
+                    <h2 className="font-bold text-xl mt-2">Ders ve Sınav Programı Planlama Sistemi</h2>
+                    <p className="px-2">A modern web application built with TypeScript, Node.js, React, and Tailwind CSS. Users can easily plan and manage course and exam schedules. The system automatically detects scheduling conflicts and, with its user-friendly interface, streamlines the planning process efficiently.</p>
                     <button>Detayları Gör</button>
                 </section>
             </div>
